7011b8b2c826dbaacf601cd2fd910fc9816fa3f0
Abdelaziz Elrahsed <aeemh.sdn@gmail.com>
2015-06-01 15:47:48 +0300

    test commit from Xcode

--- PlaySoundsViewController.swift
+++ PlaySoundsViewController.swift
@@ -19,7 +19,8 @@ class PlaySoundsViewController: UIViewController {
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        
+
+//
 //        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3"){
 //            
 //            var URL = NSURL.fileURLWithPath(filePath)!
